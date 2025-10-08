70fafea6a56e7ace473f92db501956a7efa283a1
jundellagbo <jundellagbo10dd@gmail.com>
2025-10-08 15:58:20 +0800

    initial commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Gravity Forms Kicbac Payment
+ * Plugin URI: https://gravityforms.com
+ * Description: Kicbac Payment Gateway for Gravity Forms by MBNDev
+ * Version: 1.0.0
+ * Author: MBNDev
+ * Author URI: marketing@mybizniche.com
+ * License: GPL-2.0+
+ * Text Domain: gravityform-kicbac-payment
+ *
+ */
+
